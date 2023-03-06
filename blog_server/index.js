780fed41317d12388afb297d80b8c63241444875
Artem Kodesnikov <artiguard@gmail.com>
2023-03-06 20:09:00 +0200

    delete cors option

--- blog_server/index.js
+++ blog_server/index.js
@@ -6,17 +6,11 @@ const userRoute = require('./routers/userRouter');
 const postsRoute = require('./routers/postRouter');
 const session = require('express-session');
 
-const BASE_URL = 'https://fs-blog-server.onrender.com'
 const PORT = process.env.PORT || 5000
-const corsOptions ={
-  origin: BASE_URL, 
-  credentials:true,
-  optionSuccessStatus:200
-}
 
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(session({
   secret: "thisismysecretkey",
   cookie: {
