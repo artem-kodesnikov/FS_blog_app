048f0be18c4d59b0c460dd9d6a3d49b27b0c3e46
Artem <artiguard@gmail.com>
2023-03-08 17:29:25 +0200

    another fix cors option

--- blog_server/index.js
+++ blog_server/index.js
@@ -6,19 +6,10 @@ const userRoute = require('./routers/userRouter');
 const postsRoute = require('./routers/postRouter');
 const session = require('express-session');
 
-const BASE_URL = 'https://artem-kodesnikov.github.io';
-// const BASE_URL = 'http://localhost:3000';
-
-const PORT = process.env.PORT || 5000
-const corsOptions ={
-  origin: BASE_URL,
-  credentials:true,
-  optionSuccessStatus:200
-}
-
 const app = express();
+const PORT = process.env.PORT || 5000
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(session({
   secret: "thisismysecretkey",
   cookie: {
